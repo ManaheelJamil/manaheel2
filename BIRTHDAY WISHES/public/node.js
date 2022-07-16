fce8753c94edcfd7d30667dcbc34668008836e23
Manaheel jamil <96910538+ManaheelJamil@users.noreply.github.com>
2022-07-16 08:28:10 -0700

    Add files via upload

---
+++ BIRTHDAY WISHES/public/node.js
@@ -0,0 +1,21 @@
+function heart1(){
+alert("Happy Birthday jan, I Love You Soo Much\r\nAMEEN");
+}
+function heart2() {
+alert("ALLAHPAK APKI SMILE KO HAMSHA SALAMAT RAKHY\r\nAMEEN");
+}
+function heart3(){
+alert("INSHALLAH, FARHAN APKY SARY DREAM COMPLETE HNGY\r\nAMEEN");
+}
+function heart4() {
+    alert("APKI ZINDAGI MY KABHI KOI GHUM NA HO\r\nAMEEN");
+}
+function heart5() {
+    alert("APNI BV KO BHOT SARI DSHOPPING KARWAO\r\nAMEEN");
+}
+function heart6() {
+    alert("APNI BV KO BAHAR KA KHANA KHANY SY KABHI NA RUKO\r\nAMEEN");
+}
+function manaheelLoris() {
+    var expandedParagraph = " SO,Finally My precious person birthday is come. Thanks Partner for come in my life and makeing my life is very beautiful. AllAH PAK APKO BHOT KHUSHIYAN DIKHAYN.ALLAH PAK KABHI APKI ZINDAGI MY KOI GHUM NA HO. THANKS PARTNER MUJHY ITNY KHUSHIYAN DANY KY LIY MARY LIFE  MY ITNA RELAX FEEL KARWANY KYLIY .YOU ARE THE BEST .THANKS MARY ULTY ULTY HARKATY JHALNY KYLIY.THANK MUJHY MNANANY KYLIY.MUJJY SUPPORT KARNY KYLIY.OR HR WAQT MARY SATH RAHNY KYLIY.MUJHY HANSANY KYLIY.OR UNSBKY LIY JO ABHI TK APNY MARY LIYA KIA HY.Farhan tum mary wo khushi ho jo my kisi or ky sath share nh karna chahon  g.Tum mary liy bhot special ho jan.allah pak kabhi koi ghum na ay apki zindagi my.Manaheel ka dil, manaheel ki soul, manaheel ka blood, manaheel ki hr chiz apki hy jan .manaheel pori apki hy or apki hi rahy g .Ilove you soo much mary jan mara dil mary soul.I love you soo much. and i missing you soo much hubby.and once again mary jan Happy birthday to you!!!";document.getElementById("noorainLoris").innerHTML = expandedParagraph;
+   }
